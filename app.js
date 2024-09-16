1ef20200a9671d7bef77b561f9d2e4467b6b9d22
cetmulearnrvc <rosha3varghese@gmail.com>
2024-09-16 13:33:16 +0530

    Exercise 2

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const http=require('http')
 const fs=require('fs')
 http.createServer(function (req,res)
 {
+    if(req.url==='/')
+    {
     fs.readFile('welcome.html', function(err,data)
     {
         res.writeHeader(200,{'Content-Type':'text/html'})
         res.write(data)
         res.end()
     })
+    }
+else if(req.url==='/exercise-2-redirected')
+{
+    res.write("REDIRECTED")
+    res.end()
+}
     
     
 }).listen(1000)
